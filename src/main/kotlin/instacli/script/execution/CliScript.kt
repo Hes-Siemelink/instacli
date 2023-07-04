89e427884ccea19fcb083163f9a17ee024e7a37a
Hes Siemelink <hsiemelink@digital.ai>
2023-07-04 21:12:37 +0200

    Code cleanup

--- src/main/kotlin/instacli/script/execution/CliScript.kt
+++ src/main/kotlin/instacli/script/execution/CliScript.kt
@@ -2,19 +2,34 @@ package instacli.script.execution
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
+import instacli.script.commands.ScriptInfoHandler
 import instacli.util.Yaml
 
-class CliScript(private val script: List<Command>) {
+class CliScript(val commands: List<Command>) {
+
+    val description: String? by lazy { findDescription() }
 
     fun run(context: ScriptContext): JsonNode? {
         var output: JsonNode? = null
 
-        for (command in script) {
+        for (command in commands) {
             val handler = context.getCommandHandler(command.name)
             output = command.run(handler, context)
         }
 
         return output
+    }
+
+    private fun findDescription(): String? {
+        for (command in commands) {
+            if (command.name == ScriptInfoHandler().name) {
+                val info = CliScriptInfo.from(command.data)
+                if (info.description.isNotEmpty()) {
+                    return info.description
+                }
+            }
+        }
+        return null
     }
 
     companion object {
--- src/main/kotlin/instacli/script/files/CliScriptFile.kt
+++ src/main/kotlin/instacli/script/files/CliScriptFile.kt
@@ -2,7 +2,6 @@ package instacli.script.files
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
-import instacli.script.commands.ScriptInfoHandler
 import instacli.script.execution.*
 import instacli.util.Yaml
 import java.io.File
@@ -10,28 +9,17 @@ import java.io.File
 class CliScriptFile(private val scriptFile: File) : CommandInfo, CommandHandler(asScriptCommand(scriptFile.name)) {
 
     override val name: String = asCliCommand(scriptFile.name)
-    override val description: String by lazy { findDescription() }
+    override val description: String by lazy { cliScript.description ?: asScriptCommand(name) }
 
+    private val cliScript by lazy { CliScript.from(scriptNodes) }
     private val scriptNodes: List<JsonNode> by lazy { Yaml.parse(scriptFile) }
-
-    private fun findDescription(): String {
-        for (node in scriptNodes) {
-            if (node.has(ScriptInfoHandler().name)) {
-                val value = node.get(ScriptInfoHandler().name)?.get("description")?.textValue()
-                if (value != null) {
-                    return value
-                }
-            }
-        }
-        return asScriptCommand(name)
-    }
 
     override fun execute(data: JsonNode, context: ScriptContext): JsonNode? {
         return run(VariableScope(data, context))
     }
 
     fun run(context: ScriptContext = ScriptDirectoryContext(scriptFile)): JsonNode? {
-        return CliScript.from(scriptNodes).run(context)
+        return cliScript.run(context)
     }
 }
 
