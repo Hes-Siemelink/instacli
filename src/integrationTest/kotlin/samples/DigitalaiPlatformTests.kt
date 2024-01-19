b03a412e7bfea7f06aaaa7060aa1fff96b914dfa
Hes Siemelink <hsiemelink@digital.ai>
2024-01-19 21:20:59 +0100

    Code cleanup

--- src/integrationTest/kotlin/samples/DigitalaiPlatformTests.kt
+++ src/integrationTest/kotlin/samples/DigitalaiPlatformTests.kt
@@ -17,7 +17,7 @@ class DigitalaiPlatformTests {
     private fun test(resource: String) {
         Assumptions.assumeTrue(
             TestPaths.TEST_CONNECTIONS.exists(),
-            "\nTest needs missing file: ${TestPaths.TEST_CONNECTIONS}"
+            "Missing file: ${TestPaths.TEST_CONNECTIONS}"
         )
 
         val file = toPath(resource)
