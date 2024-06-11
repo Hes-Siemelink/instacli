7edb85527703eae8edd112ee090812f561fa78c5
Hes Siemelink <hsiemelink@digital.ai>
2024-06-11 19:53:09 +0200

    Kotlin 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ group = "hes.instacli"
 version = "0.0.1-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "1.9.20"
-    kotlin("plugin.serialization") version "1.9.20"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 kotlin {
