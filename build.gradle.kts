9ae4a642c168020560b202f41d08e4fc47a6b272
Hes Siemelink <hsiemelink@digital.ai>
2024-11-14 21:30:21 +0100

    Library upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ group = "hes.instacli"
 version = "0.2.0-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "2.0.20"
-    kotlin("plugin.serialization") version "2.0.20"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 kotlin {
@@ -14,7 +14,7 @@ repositories {
     mavenLocal()
     mavenCentral()
     maven {
-        url = uri("https://repo1.maven.org")
+        url = uri("https://jitpack.io")
     }
 }
 
@@ -25,16 +25,16 @@ dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.+")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.+")
     implementation("com.networknt:json-schema-validator:1.4.0")
-    implementation("io.ktor:ktor-client-core:2.3.+")
-    implementation("io.ktor:ktor-client-java:2.3+")
-    implementation("io.ktor:ktor-client-auth:2.3+")
+    implementation("io.ktor:ktor-client-core:3.0.+")
+    implementation("io.ktor:ktor-client-java:3.0+")
+    implementation("io.ktor:ktor-client-auth:3.0+")
     implementation("org.slf4j:slf4j-simple:2.0.+")
     implementation("com.github.kotlin-inquirer:kotlin-inquirer:0.1.0")
-    implementation("org.jline:jline:3.22.0")
-    implementation("org.fusesource.jansi:jansi:2.4.0")
-    implementation("com.lordcodes.turtle:turtle:0.8.0")
-    implementation("io.javalin:javalin:6.1.+")
-    implementation("org.xerial:sqlite-jdbc:3.46.1.0")
+    implementation("org.jline:jline:3.27.+")
+    implementation("org.fusesource.jansi:jansi:2.4.1")
+    implementation("com.lordcodes.turtle:turtle:0.10.0")
+    implementation("io.javalin:javalin:6.3.+")
+    implementation("org.xerial:sqlite-jdbc:3.47.0.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testImplementation("io.kotest:kotest-assertions-core:5.7.2")
