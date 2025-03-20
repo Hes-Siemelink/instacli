dbac13870d41656949e101cbab0494ee98afe504
Hes Siemelink <hsiemelink@digital.ai>
2025-03-20 19:25:14 +0100

    Removed Kotlin project from easyspec directory

--- easyspec/settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-rootProject.name = "easyspec"
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.9.0")
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,5 @@
 rootProject.name = "instacli"
 
-includeBuild("easyspec")
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.9.0")
 }
