48327a93f03a7d42ea259111ab90731ed9baeae3
Hes Siemelink <hsiemelink@digital.ai>
2025-04-05 08:50:59 +0200

    ✨ 'Write file' command: takes 'file' and 'content' parameters

--- instacli-spec/commands/instacli/files/Write file.spec.md
+++ instacli-spec/commands/instacli/files/Write file.spec.md
@@ -10,15 +10,31 @@
 
 [Write file.schema.yaml](schema/Write%20file.schema.yaml)
 
-## Basic usage
+## Writing content to a file
 
-Use **Write file** to store the contents of the `${output}` variable in a file.
+Use **Write file** to store content in a file
+
+```yaml instacli
+Code example: Write content to a file
+
+Write file:
+  file: out/greeting.txt
+  content: Hello, World!
+
+Read file: out/greeting.txt
+
+Expected output: Hello, World!
+```
+
+## Short version
+
+You can store the contents of the `${output}` variable directly with the short form.
 
 ```yaml instacli
 Code example: Save output
 
-Output: |
-  Hello, World!
+Output: Hello, World!
 
 Write file: out/greeting.txt
 ```
+
--- src/main/kotlin/instacli/commands/files/WriteFile.kt
+++ src/main/kotlin/instacli/commands/files/WriteFile.kt
@@ -1,24 +1,41 @@
 package instacli.commands.files
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.databind.node.ValueNode
-import instacli.language.CommandHandler
-import instacli.language.ScriptContext
-import instacli.language.ValueHandler
+import instacli.language.*
 import instacli.util.toDisplayYaml
 import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.io.path.createParentDirectories
 
-object WriteFile : CommandHandler("Write file", "instacli/files"), ValueHandler {
+object WriteFile : CommandHandler("Write file", "instacli/files"), ValueHandler, ObjectHandler {
 
     override fun execute(data: ValueNode, context: ScriptContext): JsonNode? {
 
-        val destinationFile = Path.of(data.textValue())
-        destinationFile.createParentDirectories()
-
-        Files.writeString(destinationFile, context.output.toDisplayYaml())
+        val filename = data.textValue()
+        val content = context.output ?: throw InstacliCommandError(
+            "Write file requires 'content' parameter or non-null output variable."
+        )
+        writeFile(filename, content)
 
         return null
+    }
+
+    override fun execute(data: ObjectNode, context: ScriptContext): JsonNode? {
+        val filename = data.getParameter("file").textValue()
+        val content = data["content"] ?: context.output ?: throw InstacliCommandError(
+            "Write file requires 'content' parameter or non-null output variable."
+        )
+
+        writeFile(filename, content)
+
+        return null
+    }
+
+    private fun writeFile(filename: String, content: JsonNode) {
+        val destinationFile = Path.of(filename)
+        destinationFile.createParentDirectories()
+        Files.writeString(destinationFile, content.toDisplayYaml())
     }
 }
