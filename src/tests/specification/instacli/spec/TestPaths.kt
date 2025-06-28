e6d9c04d9e27b785c158870e7713ba326c41dfdc
Hes Siemelink <hsiemelink@digital.ai>
2025-06-28 08:29:50 +0200

    Code cleanup

--- src/tests/specification/instacli/spec/TestPaths.kt
+++ src/tests/specification/instacli/spec/TestPaths.kt
@@ -7,7 +7,4 @@ object TestPaths {
     val SPEC: Path = Path.of("instacli-spec")
 
     val SAMPLE_SERVER: Path = Path.of("samples/http-server/sample-server/sample-server.cli")
-
-    val RESOURCES: Path = Path.of("src/tests/resources")
-    val TEST_CREDENTIALS: Path = RESOURCES.resolve("instacli-home/credentials.yaml")
 }
